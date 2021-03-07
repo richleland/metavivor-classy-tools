f4edf601658d1e77856ac0ca3ff6a623ed151c30
Rich Leland <rich.leland@avaaz.org>
2021-03-07 18:26:16 -0500

    add command to get list of all individual pages

--- api.py
+++ api.py
@@ -35,7 +35,7 @@ def get_campaigns_from_api(org_id, url=None):
     return campaigns
 
 
-def get_teams_from_api(campaign_id, url=None):
+def get_fundraising_teams_from_api(campaign_id, url=None):
     if not url:
         url = f"{API_URL}/campaigns/{campaign_id}/fundraising-teams"
 
@@ -49,10 +49,30 @@ def get_teams_from_api(campaign_id, url=None):
     # see if there's a next page
     next_page_url = response_json["next_page_url"]
     if next_page_url:
-        teams = teams + get_teams_from_api(campaign_id, next_page_url)
+        teams = teams + get_fundraising_teams_from_api(campaign_id, next_page_url)
 
     # return the teams from all pages
     return teams
+
+
+def get_fundraising_pages_from_api(campaign_id, url=None):
+    if not url:
+        url = f"{API_URL}/campaigns/{campaign_id}/fundraising-pages"
+
+    response = session.get(url)
+    response.raise_for_status()
+    response_json = response.json()
+
+    # append the current page's data
+    pages = response_json["data"]
+
+    # see if there's a next page
+    next_page_url = response_json["next_page_url"]
+    if next_page_url:
+        pages = pages + get_fundraising_pages_from_api(campaign_id, next_page_url)
+
+    # return the pages from all pages
+    return pages
 
 
 # # get a transaction
--- classy.py
+++ classy.py
@@ -1,6 +1,6 @@
 import click
 
-from api import get_teams_from_api, get_campaigns_from_api
+from api import get_campaigns_from_api, get_fundraising_pages_from_api, get_fundraising_teams_from_api
 from config import ORG_ID
 
 
@@ -20,15 +20,25 @@ def list_campaigns():
 
 @click.command()
 def list_teams():
-    teams = get_teams_from_api(319115)
+    teams = get_fundraising_teams_from_api(319115)
     click.echo(f"Number of teams: {len(teams)}")
     click.echo("The following teams are in Classy:")
     for team in teams:
         click.echo(f"[{team['id']}] {team['name']}")
 
 
+@click.command()
+def list_pages():
+    pages = get_fundraising_pages_from_api(319115)
+    click.echo(f"Number of pages: {len(pages)}")
+    click.echo("The following pages are in Classy:")
+    for page in pages:
+        click.echo(f"[{page['id']}] {page['alias']}")
+
+
 cli.add_command(list_campaigns)
 cli.add_command(list_teams)
+cli.add_command(list_pages)
 
 
 if __name__ == "__main__":
