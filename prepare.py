14e5d89ae83164bb0757465f44d212a7a996a616
Rich Leland <rich.leland@avaaz.org>
2021-03-07 14:52:36 -0500

    use built-in csv library to prepare data

--- prepare.py
+++ prepare.py
@@ -1,55 +1,46 @@
-import pandas as pd
+import csv
+import json
+from datetime import datetime
 
+formatted = []
+with open("input/checks-2021-03-07.csv") as csvfile:
+    reader = csv.DictReader(csvfile)
 
-df = pd.read_csv("input/checks-2021-03-07.csv", dtype=str)
+    for row in reader:
+        converted = {
+            "billing_first_name": row["Donor First Name"] or None,
+            "billing_last_name": row["Donor Last Name"] or None,
+            "billing_address1": row["Billing Address 1"] or None,
+            "billing_address2": row["Billing Address 2"] or None,
+            "billing_city": row["Billing City"] or None,
+            "billing_state": row["Billing State"] or None,
+            "billing_postal_code": row["Billing Postal Code"] or None,
+            "billing_country": row["Billing Country"] or None,
+            "company_name": row["Company Name"] or None,
+            "comment": row["special handling"] or None,
+            "fundraising_page_id": row["Individual Page ID"] or None,
+            "items": [
+                {
+                    "overhead_amount": 0,
+                    "price": float(row["Gross Transaction Amount"]),
+                    "product_id": None,
+                    "product_name": "Offline Donation",
+                    "quantity": 1,
+                    "type": "donation",
+                }
+            ],
+            "member_email_address": row["Billing Email Address"] or None,
+            "member_phone": row["donor phone"] or None,
+            "offline_payment_info": {
+                "check_number": row["check number"],
+                "description": "Check donation",
+                "payment_type": "check",
+                "sync_third_party": True,
+            },
+            "purchased_at": datetime.strptime(row["Transaction Date"], "%m/%d/%y").strftime("%Y-%m-%dT12:00:00-0500"),
+        }
 
-formatted = pd.DataFrame(
-    [],
-    columns=[
-        "purchased_at",
-        "fundraising_team_id",
-        "fundraising_page_id",
-        "comment",
-        "billing_first_name",
-        "billing_last_name",
-        "billing_address1",
-        "billing_address2",
-        "billing_city",
-        "billing_state",
-        "billing_postal_code",
-        "billing_country",
-        "member_email_address",
-        "company_name",
-        "price",
-        "check_number",
-    ],
-    dtype=str,
-)
+        formatted.append(converted)
 
-# transaction metadata
-formatted["fundraising_team_id"] = df["Team Page ID"]
-formatted["fundraising_page_id"] = df["Individual Page ID"]
-formatted["comment"] = df["special handling"]
-formatted["purchased_at"] = df["Transaction Date"]
-formatted["purchased_at"] = pd.to_datetime(formatted["purchased_at"])
-formatted["purchased_at"] = formatted["purchased_at"].dt.strftime("%Y-%m-%dT12:00:00-0500")
-
-# contact information
-formatted["billing_first_name"] = df["Donor First Name"]
-formatted["billing_last_name"] = df["Donor Last Name"]
-formatted["billing_address1"] = df["Billing Address 1"]
-formatted["billing_address2"] = df["Billing Address 2"]
-formatted["billing_city"] = df["Billing City"]
-formatted["billing_state"] = df["Billing State"]
-formatted["billing_postal_code"] = df["Billing Postal Code"]
-formatted["billing_country"] = df["Billing Country"]
-formatted["member_email_address"] = df["Billing Email Address"]
-formatted["member_phone"] = df["donor phone"]
-formatted["company_name"] = df["Company Name"]
-
-# payment info
-formatted["price"] = df["Gross Transaction Amount"]
-formatted["price"] = formatted["price"].astype(float)
-formatted["check_number"] = df["check number"]
-
-formatted.to_json("output/checks-2021-03-07.json", orient="records", indent=2)
+with open("output/checks-2021-03-07.json", "w") as f:
+    json.dump(formatted, f, indent=2)
