9f8288944c203c31d7c1cffc2fb07e6619bfe334
Rich Leland <rich.leland@avaaz.org>
2022-02-13 11:14:56 -0500

    stub out form for upload

--- app.py
+++ app.py
@@ -1,18 +1,22 @@
-from flask import Flask
+from flask import Flask, redirect, render_template
 
 from api import get_campaigns_from_api
-from config import ORG_ID
+from forms import UploadForm
 
 app = Flask(__name__)
+app.config.from_object("config")
 
 
-@app.route("/")
-def index():
-    return "Hello world!"
+@app.route("/upload")
+def upload():
+    form = UploadForm()
+    if form.validate_on_submit():
+        return redirect("/")
+    return render_template("upload.html", form=form)
 
 
 @app.route("/campaigns")
 def campaigns_list():
-    campaigns = get_campaigns_from_api(ORG_ID)
+    campaigns = get_campaigns_from_api(app.config["ORG_ID"])
     campaigns = [{k: v for k, v in c.items() if k in ["id", "name", "canonical_url"]} for c in campaigns]
     return {"campaigns": campaigns}
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ import os
 CLIENT_ID = os.environ["CLASSY_CLIENT_ID"]
 CLIENT_SECRET = os.environ["CLASSY_CLIENT_SECRET"]
 ORG_ID = os.environ["CLASSY_ORG_ID"]
+SECRET_KEY = os.environ["SECRET_KEY"]
 DEFAULT_CAMPAIGN_ID = os.environ["CLASSY_DEFAULT_CAMPAIGN_ID"]
 TOKEN_URL = "https://api.classy.org/oauth2/auth"
 API_URL = "https://api.classy.org/2.0"
