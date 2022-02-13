54fcffe0120eef21ce4c6f24315e88e769b110fc
Rich Leland <rich.leland@avaaz.org>
2022-02-13 11:01:32 -0500

    test listing of campaigns from API using Flask

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
 from flask import Flask
 
+from api import get_campaigns_from_api
+from config import ORG_ID
+
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
     return "Hello world!"
+
+
+@app.route("/campaigns")
+def campaigns_list():
+    campaigns = get_campaigns_from_api(ORG_ID)
+    campaigns = [{k: v for k, v in c.items() if k in ["id", "name", "canonical_url"]} for c in campaigns]
+    return {"campaigns": campaigns}
