e2dd4f2cc72a2f80fc777f984455cad1e45c58fb
Rich Leland <rich.leland@avaaz.org>
2022-02-13 11:43:02 -0500

    modify app to create instance

--- app/classy_tools/app.py
+++ app/classy_tools/app.py
@@ -1,23 +1,24 @@
 from flask import Flask, redirect, render_template
 
-from .api import get_campaigns_from_api
-from .forms import UploadForm
+from classy_tools.api import get_campaigns_from_api
+from classy_tools.forms import UploadForm
 
 
-app = Flask(__name__)
-app.config.from_object("config")
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("classy_tools.config")
 
+    @app.route("/upload")
+    def upload():
+        form = UploadForm()
+        if form.validate_on_submit():
+            return redirect("/")
+        return render_template("upload.html", form=form)
 
-@app.route("/upload")
-def upload():
-    form = UploadForm()
-    if form.validate_on_submit():
-        return redirect("/")
-    return render_template("upload.html", form=form)
+    @app.route("/campaigns")
+    def campaigns_list():
+        campaigns = get_campaigns_from_api(app.config["ORG_ID"])
+        campaigns = [{k: v for k, v in c.items() if k in ["id", "name", "canonical_url"]} for c in campaigns]
+        return {"campaigns": campaigns}
 
-
-@app.route("/campaigns")
-def campaigns_list():
-    campaigns = get_campaigns_from_api(app.config["ORG_ID"])
-    campaigns = [{k: v for k, v in c.items() if k in ["id", "name", "canonical_url"]} for c in campaigns]
-    return {"campaigns": campaigns}
+    return app
